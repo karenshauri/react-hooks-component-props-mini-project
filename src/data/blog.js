ff11aaca807c790d8a8d52cc6d76d562ae977fd0
karenshauri <karennduta12@gmail.com>
2025-07-27 23:28:49 +0300

    commit

--- src/data/blog.js
+++ src/data/blog.js
@@ -1,31 +1,29 @@
-import logo from "../assets/logo";
+export const blogName = "Overreacted";
 
-const blogData = {
-  name: "Underreacted",
-  image: logo,
-  about: "A blog about learning React",
-  posts: [
-    {
-      id: 1,
-      title: "Components 101",
-      date: "December 15, 2020",
-      preview: "Setting up the building blocks of your site",
-      minutes: 5,
-    },
-    {
-      id: 2,
-      title: "React Data Flow",
-      date: "December 11, 2020",
-      preview: "Passing props is never passé",
-      minutes: 15,
-    },
-    {
-      id: 3,
-      title: "Function vs Class Components",
-      preview: "React, meet OOJS.",
-      minutes: 47,
-    },
-  ],
-};
+export const about = "Personal blog by Dan Abramov. I explain with words and code.";
 
-export default blogData;
+export const image = "https://via.placeholder.com/215";
+
+export const posts = [
+  {
+    id: 1,
+    title: "The WET Codebase",
+    date: "July 13, 2020",
+    preview: "Come waste your time with me.",
+    minutes: 5,
+  },
+  {
+    id: 2,
+    title: "Goodbye, Clean Code",
+    date: "January 11, 2020",
+    preview: "Let clean code guide you. Then let it go.",
+    minutes: 22,
+  },
+  {
+    id: 3,
+    title: "My Decade in Review",
+    date: "January 1, 2020",
+    preview: "A personal reflection.",
+    minutes: 65,
+  },
+];
